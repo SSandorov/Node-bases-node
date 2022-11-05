6f12bfd64e47f101f3a043ac8dcab3fca50c2105
ssandorov <49904591+SSandorov@users.noreply.github.com>
2022-11-05 17:00:19 +0100

    uso del modulo file system de node

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-// Tarea: mostrar en consola la tabla de multiplicar de 5 hasta 10
+// Tarea: crear un .txt con la tabla de multiplicar
+
+// Para ello empleamos el módulo file system de node usnado el require()
+const fs = require('fs');
+
+
 
 // limpiamos la consola
 console.clear();
 
+const base = 7;
+let salida = '';
+
 // Añadimos un poco de estilos 'manuales'
 console.log('================================');
-console.log('           Tabla del 5');
+console.log(`           Tabla del ${base}`);
 console.log('================================');
+
 
 // ejecutamos el bucle for
 const tabla5 = () => {
-    const base = 5;
+
+    // creamos una variable que nos almacena los valores del bucle
 
     for (let i = 1; i <= 10; i++) {
         const valor = base * i;
-        console.log(`${base} x ${i} = ${valor}`);      
+        salida += `${base} x ${i} = ${valor}\n`;    
     }
+
+    console.log(salida);
 }
 
 tabla5();
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+    if(err) throw err;
+
+    console.log(`tabla-${base}.txt creada`);
+})
